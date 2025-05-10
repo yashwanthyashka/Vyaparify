2aac439dbf781dd4512cf5351bcd25a5ce02b7f6
Yashwanth <officialyash2502@gmail.com>
2025-05-10 12:51:33 +0530

    Update cloudinary.js

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'dy4r3s6pi',
-  api_key: '773239769322537',
-  api_secret: '1Puw_0MGb0xURfxet7XfAT4LxS8',
+  cloud_name: 'your-cloud-name',
+  api_key: 'your-api-key',
+  api_secret: 'your-secret-key',
 });
 
 module.exports = cloudinary;
